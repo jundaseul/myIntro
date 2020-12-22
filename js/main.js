ff179cc3ff4e1ac3ea97a0235717eeb54de0a9de
jundaseul <jjdaseul_@naver.com>
2020-12-22 20:28:08 +0900

    수정본(12/22)

--- js/main.js
+++ js/main.js
@@ -97,7 +97,7 @@ $(function () { /////// jQB ///////////////////////
 
     /*인트로 마우스 커서 ****************************************/
     // 포인터가 움직일 대상 : .book
-    $(".book").mousemove(function (e) { // e-이벤트전달
+    $(".book.intro").mousemove(function (e) { // e-이벤트전달
         var posx = e.pageX + 5; //하단클릭을 위해 조금 벗어나게 +5 해줌!! 
         var posy = e.pageY;
         //console.log("x축:"+posx+"\ny축:"+posy);
@@ -113,7 +113,7 @@ $(function () { /////// jQB ///////////////////////
 
 
     // 이벤트: mouseenter -> 마우스가 영역안으로 들어갈 때만 처리하면 됨
-    $(".book").mouseenter(function () {
+    $(".book.intro").mouseenter(function () {
 
         // console.log("순번:"+idx);
 
@@ -122,7 +122,7 @@ $(function () { /////// jQB ///////////////////////
 
     }); /////// mouseenter /////////////
 
-    $(".book").mouseleave(function () {
+    $(".book.intro").mouseleave(function () {
 
         // console.log("순번:"+idx);
 
@@ -133,58 +133,26 @@ $(function () { /////// jQB ///////////////////////
 
 
     /*인트로 누르면 다음페이지로 이동*/
-    $(".book").click(function () {
+    $(".book.intro").click(function () {
         location.href = "page_1.html"
     });
 
 
 
-    /*타이핑 효과 **********************************/
-    var typingBool = false;
-    var typingIdx = 0;
-    var liIndex = 0;
-    var liLength = $(".typing-txt>ul>li").length;
-
-    // 타이핑될 텍스트를 가져온다 
-    var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
-    typingTxt = typingTxt.split(""); // 한글자씩 자른다. 
-    if (typingBool == false) { // 타이핑이 진행되지 않았다면 
-        typingBool = true;
-        var tyInt = setInterval(typing, 100); // 반복동작 
-    }
-
-    function typing() {
-        $(".typing ul li").removeClass("on");
-        $(".typing ul li").eq(liIndex).addClass("on");
-        if (typingIdx < typingTxt.length) { // 타이핑될 텍스트 길이만큼 반복 
-            $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-            typingIdx++;
-        } else {
-            if (liIndex < liLength - 1) {
-                //다음문장으로  가기위해 인덱스를 1증가
-                liIndex++;
-                //다음문장을 타이핑하기위한 셋팅
-                typingIdx = 0;
-                typingBool = false;
-                typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
-
-                //다음문장 타이핑전 1초 쉰다
-                clearInterval(tyInt);
-                //타이핑종료
-
-                setTimeout(function () {
-                    //1초후에 다시 타이핑 반복 시작
-                    tyInt = setInterval(typing, 100);
-                }, 1000);
-            } else if (liIndex == liLength - 1) {
-
-                //마지막 문장까지 써지면 반복종료
-                clearInterval(tyInt);
-            }
-        }
-    }
-
-
+    /*공동 프로젝트 ***************************************/
+    $(".proj").mouseenter(function () {
+        $(this).css({
+                backgroundColor: "#e9e5db"
+            });
+        $(this).addClass("on").siblings().removeClass("on");      
+    });
+    
+    $(".proj").mouseleave(function () {
+        $(this).css({
+                backgroundColor: "transparent"
+            });
+        $(this).removeClass("on");      
+    });
 
 
 
