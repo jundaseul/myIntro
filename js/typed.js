d1403842801449ccba58d5966c7eac7cd9359444
jundaseul <jjdaseul_@naver.com>
2021-01-13 18:02:33 +0900

    새 파일 업데이트

---
+++ js/typed.js
@@ -0,0 +1,19 @@
+$(function(){ /////// jQB ///////////////////////
+    //console.log("로딩완료!");
+    
+    
+    
+    /*타이핑 효과 **********************************/
+    var typed5 = new Typed('#typed5', {
+        strings: ['안녕하세요.<br>저의 포트폴리오를 소개합니다!'],
+        typeSpeed: 85,
+        backSpeed: 0,
+        cursorChar: '_',
+        shuffle: true,
+        smartBackspace: false,
+        loop: false
+    });
+    
+    
+});////////// jQB ///////////////////////////////
+/////////////////////////////////////////////////
---
+++ js/typed2.js
@@ -0,0 +1,15 @@
+$(function(){ /////// jQB ///////////////////////
+    console.log("로딩완료!");
+    
+    
+    var typed5 = new Typed('#typed5', {
+        strings: ['저의 포트폴리오를 봐주셔서<br>감사합니다 :-)'],
+        typeSpeed: 85,
+        backSpeed: 0,
+        cursorChar: '_',
+        shuffle: true,
+        smartBackspace: false,
+        loop: false
+    });
+});////////// jQB ///////////////////////////////
+/////////////////////////////////////////////////
